faae576ee4aed847a24edc8b1642ea537578d9c0
Divyanshu <divyiitian2019@gmail.com>
2022-08-12 20:43:48 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Management enthusiast","SDE enthusiast","Web Developer","Video editor","Competitive Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
